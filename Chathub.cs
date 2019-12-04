6765e4886822dd4ae0b5ad8e163d73f137393fb9
zhang <zhang@DESKTOP-P61J3IO>
2019-12-04 21:29:07 +0800

    添加项目文件。

---
+++ Chathub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IM
+{
+    public class ChatHub:Hub
+    {
+        
+        public Task SendMsg(string username,string message)
+        {
+            return Clients.All.SendAsync("Show", username , message);
+        }
+        //public Task SendMsgToUser(int userid,string message)
+        //{
+        //    return Clients.Client(userid).SendAsync();
+        //}
+
+        
+    }
+}
